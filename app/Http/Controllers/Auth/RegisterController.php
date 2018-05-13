225c8d6793770e0725adb054b1c60bdb041229ff
Alexey <alexey.lighthouse@gmail.com>
2018-05-13 18:41:14 +0300

    added signup verification

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use App\Mail\Auth\VerifyMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -40,6 +44,26 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
+    public function verify($token)
+    {
+        if (!$user = User::where('verify_token', $token)->first()) {
+            return redirect()->route('login')
+                ->with('error', 'Sorry your link cannot be identified.');
+        }
+
+        if ($user->status !== User::STATUS_WAIT) {
+            return redirect()->route('login')
+                ->with('error', 'Your email is already verified.');
+        }
+
+        $user->status = User::STATUS_ACTIVE;
+        $user->verify_token = null;
+        $user->save();
+
+        return redirect()->route('login')
+            ->with('success', 'Your e-mail is verified. You can now login.');
+    }
+
     /**
      * Get a validator for an incoming registration request.
      *
@@ -58,15 +82,21 @@ class RegisterController extends Controller
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
-     * @return \App\User
+     * @param array $data
+     * @return $this|\Illuminate\Database\Eloquent\Model
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'verify_token' => Str::random(),
+            'status' => User::STATUS_WAIT,
         ]);
+
+        Mail::to($user->email)->send(new VerifyMail($user));
+
+        return $user;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,7 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
 
+Route::get('/verify/{token}', 'Auth\RegisterController@verify')->name('register.verify');
+
 Auth::routes();
 
