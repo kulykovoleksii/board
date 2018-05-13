2eac48d05b8b70fbcb92930477551d364ad22108
Alexey <alexey.lighthouse@gmail.com>
2018-05-13 22:14:42 +0300

    added admin page

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,34 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', 'HomeController@index')->name('home');
-
-Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
-
-Route::get('/verify/{token}', 'Auth\RegisterController@verify')->name('register.verify');
 
 Auth::routes();
 
+Route::get('/verify/{token}', 'Auth\RegisterController@verify')->name('register.verify');
+
+Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
+
+Route::group(
+    [
+        'prefix' => 'admin',
+        'as' => 'admin.',
+        'namespace' => 'Admin',
+        'middleware' => ['auth'],
+    ],
+    function () {
+        Route::get('/', 'HomeController@index')->name('home');
+    }
+);
