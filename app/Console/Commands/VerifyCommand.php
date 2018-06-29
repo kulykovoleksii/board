c11389e2329875652f61cc60a8c36f832c325efd
Alexey <alexey.lighthouse@gmail.com>
2018-06-29 22:18:09 +0300

    Added user verify command

--- app/Console/Commands/VerifyCommand.php
+++ app/Console/Commands/VerifyCommand.php
@@ -1,42 +1,44 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands\User;
 
+use App\Entity\User;
+use App\UseCases\Auth\RegisterService;
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
 
 class VerifyCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'command:name';
+    protected $signature = 'user:verify {email}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+    protected $description = 'Verify user email';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    private $service;
+
+    public function __construct(RegisterService $service)
     {
         parent::__construct();
+        $this->service = $service;
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
+    public function handle(): bool
     {
-        //
+        $email = $this->argument('email');
+
+        /** @var User $user */
+        if (!$user = User::where('email', $email)->first()) {
+            $this->error('Undefined user with email ' . $email);
+            return false;
+        }
+
+        try {
+            $this->service->verify($user->id);
+        } catch (\DomainException $e) {
+            $this->error($e->getMessage());
+            return false;
+        }
+
+        $this->info('User is successfully verified');
+        return true;
     }
 }
