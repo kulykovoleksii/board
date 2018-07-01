e25be0eb1cafd8352deebf706099d841032f6285
Alexey <alexey.lighthouse@gmail.com>
2018-07-01 09:29:06 +0300

    added seed for admin

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -7,6 +7,18 @@ class UsersTableSeeder extends Seeder
 {
     public function run(): void
     {
+        $user = [
+            'id' => 1,
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'remember_token' => str_random(10),
+            'verify_token' => null,
+            'role' => User::ROLE_ADMIN,
+            'status' => User::STATUS_ACTIVE,
+        ];
+        User::create($user);
+
         factory(User::class, 10)->create();
     }
 }
