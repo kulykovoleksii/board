090a644b496efa99f923a17c428ab43416a21682
kulykovoleksii <alexey.lighthouse@gmail.com>
2025-10-26 08:00:19 +0000

    Remove migrated Blade templates
    Deleted all Blade views that have been migrated to React/Inertia:
    - Admin panel views (users, pages, regions, categories, adverts, banners, tickets)
    - Cabinet views (profile, adverts, banners, tickets, favorites)
    - Public views (adverts, pages, home)
    - Auth views (login, register, password reset, phone verification)
    - Old layouts
    
    Remaining Blade files:
    - app.blade.php (Inertia root layout)
    - banner/get.blade.php (embed banner snippet)
    - emails/auth/register/verify.blade.php (email template)

--- resources/views/admin/home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    @include ('admin._nav', ['page' => ''])
-@endsection
--- resources/views/cabinet/banners/create/_categories.blade.php
+++
@@ -1,8 +0,0 @@
-<ul>
-    @foreach($categories as $category)
-        <li>
-            <a href="{{ route('cabinet.banners.create.region', $category) }}">{{ $category->name }}</a>
-            @include('cabinet.banners.create._categories', ['categories' => $category->children])
-        </li>
-    @endforeach
-</ul>
